f5a9052dac19f2c7aaf7adc96d67c5be2c03b24b
Incrested <30969346+Incrested@users.noreply.github.com>
2017-10-05 21:45:04 -0700

    Update and rename script.py to bot.py

--- bot.py
+++ bot.py
@@ -8,4 +8,10 @@ async def on_message(message):
   if messsage.content.startswith('!ping'):
     await client.send_message(message.channel, 'Your expecting me to say **Pong!**, aren't you?')
     
+@client.event
+async def on_message(message):
+  if message.content.startswith('!help'):
+    await client.send_messsage(message.author, 'The Current Command List is weak and consists only of **!ping**, **!help**, and **!music**. Music isn't working yet')
+                              
+                              
 client.run('MzY1NjQ5OTE4MTQyMDU0NDEw.DLiH3g.yrJsXqNQMeP7g6kFGN_DFo-zlts')
