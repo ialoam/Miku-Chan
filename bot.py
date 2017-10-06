31fc333ce2d3f03777f397bfce53eb0cf0f0c4c7
Incrested <30969346+Incrested@users.noreply.github.com>
2017-10-06 06:23:27 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ async def on_message(message):
 @client.event
 async def on_message(message):
   if message.content.startswith('!help'):
-    await client.send_messsage(message.author, 'The Current Command List is weak and consists only of **!ping**, **!help**, and **!music**. Music is not ready yet')
+    await client.send_messsage(message.author, 'Commands are: **!help**, **!ping**, and **!ding**')
                               
                               
 client.run('MzY1NjQ5OTE4MTQyMDU0NDEw.DLiH3g.yrJsXqNQMeP7g6kFGN_DFo-zlts')
