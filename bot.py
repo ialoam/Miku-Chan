808651209006d8b659bc448392f601fe89e0252e
Incrested <30969346+Incrested@users.noreply.github.com>
2017-10-06 06:24:42 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,5 +13,9 @@ async def on_message(message):
   if message.content.startswith('!help'):
     await client.send_messsage(message.author, 'Commands are: **!help**, **!ping**, and **!ding**')
                               
-                              
+@client.event
+async def on_message(message):
+  if message.content.startswith('!ding'):
+    await client.send_message(message.author, 'Dong!')
+      
 client.run('MzY1NjQ5OTE4MTQyMDU0NDEw.DLiH3g.yrJsXqNQMeP7g6kFGN_DFo-zlts')
