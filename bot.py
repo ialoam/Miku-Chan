2fa903f22410a69f0286750f87129d85dbede54b
Incrested <30969346+Incrested@users.noreply.github.com>
2017-10-07 10:36:47 -0700

    Changed '' to ""

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-	if message.content.startswith('$ping'):
+	if message.content.startswith("$ping"):
 		await client.send_message(message.channel, "Your expecting me to say 'Pong!', aren't you?");
     
 @client.event
