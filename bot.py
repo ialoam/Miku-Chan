c7f776cfc26c6004822d6b3231d9de27c38975f3
Incrested <30969346+Incrested@users.noreply.github.com>
2017-10-06 22:38:47 -0700

    Added new Console Items/Changed Command Prefix

--- bot.py
+++ bot.py
@@ -12,20 +12,27 @@ async def on_ready():
 	print(client.user.name);
 	print(client.user.id);
 	print('-------');
+	await client.change_status(game='Stufz. Do $help', idle=False)
 
 @client.event
 async def on_message(message):
-  if messsage.content.startswith('!ping'):
+  if messsage.content.startswith('$ping'):
     await client.send_message(message.channel, 'Your expecting me to say pong');
     
 @client.event
 async def on_message(message):
-  if message.content.startswith('!help'):
+  if message.content.startswith('$help'):
     await client.send_messsage(message.channel, 'Add a ! to ping or ding for the beautiful commands');
                               
 @client.event
 async def on_message(message):
-  if message.content.startswith('!ding'):
+  if message.content.startswith('$ding'):
     await client.send_message(message.channel, 'Dong!');
       
+@client.event
+async def on_message(message):
+  if message.content.startswith('$banned'):
+	bans = await client.get_bans(server)
+	await client.send_message(message.channel, 'Banned People: ' + server)
+	
 client.run('MzY1NjQ5OTE4MTQyMDU0NDEw.DLkWlw.bANvZcyQXhYWXvxg5pvHBflAuss')
