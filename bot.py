f4912e57dc77713dadcb85bcee9b021bd55094ad
Incrested <30969346+Incrested@users.noreply.github.com>
2017-10-07 09:45:11 -0700

    Added $fetchrole for Miku's Cave
    This will be verstile soon.. i hope.

--- bot.py
+++ bot.py
@@ -17,16 +17,41 @@ async def on_ready():
 @client.event
 async def on_message(message):
   if message.content.startswith('$ping'):
-    await client.send_message(message.channel, 'Ping?');
+    await client.send_message(message.channel, "Your expecting me to say 'Pong!', aren't you?");
     
 @client.event
 async def on_message(message):
-	if message.content.startswith('$icup'):
-		await client.send_message(message.channel, 'Ha. ha. Very funny. (not)');
+	if message.content.startswith("$icup"):
+		await client.send_message(message.channel, "Ha. ha. Very funny. (not)");
 		
 @client.event
 async def on_message(message):
-  if message.content.startswith('$ding'):
-    await client.send_message(message.channel, 'Dong!');
+  if message.content.startswith("$ding"):
+    await client.send_message(message.channel, "Dong!");
+	
+@client.event
+async def on_message(message):
+	if message.content.startswith("$fetchrole"):
+		team_list = ["Humans"]
+		entered_team = message.content[6:].lower()
+		role = discord.utils.get(message.server.roles, name=entered_team)
+		roles = [
+			"366256516220583937",
+		]
+		for r in message.author.roles:
+			if r.id in roles:
+				await client.send_message(message.channel, "You already have the role.")
+			return
+			if role is None or role.name not in team_list:
+				await client.send_message(message.channel, "Well that's odd. It didn't work, contact a Helper or Dev.")
+			return
+			elif role in message.author.roles:
+				await client.send_message(message.channel, "You already have this role.")
+			else:
+			try:
+				await client.add_roles(message.author, role)
+				await client.send_message(message.channel, "Successfully added role {0}".format(role.name))
+			except discord.Forbidden:
+				await client.send_message(message.channel, "Sorry, I need ***MANAGE_ROLES*** to do this.")
 	
 client.run('MzY1NjQ5OTE4MTQyMDU0NDEw.DLkWlw.bANvZcyQXhYWXvxg5pvHBflAuss')
