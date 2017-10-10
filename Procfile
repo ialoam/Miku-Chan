649dbf0fa81056c7c529b6f7ba752b17b3d9d570
Incrested <30969346+Incrested@users.noreply.github.com>
2017-10-09 17:45:35 -0700

    Changed from Python to Node.JS

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python bot.py
+worker: javascript bot.js
---
+++ bot.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log('Logged in as ${client.user.tag}!');
+});
+
+client.on('message', msg => {
+	if (msg.content === '$ping') {
+		msg.reply('No pinging.');
+	}
+});
+
+client.on('message', msg => {
+	if (msg.content === '$ding') {
+		msg.reply('Dong!');
+	}
+});
+
+client.on('message', msg => {
+	if (msg.content === '$cmds') {
+		msg.reply('Visit the documentation on our site for more info: http://docs.mikuchan.me');
+	}
+});
+
+client.on('message', msg => {
+	if (msg.content === '$icup') {
+		msg.reply('Ha ha. Very funny. ***(not)***.');
+	}
+});
+
+
+
+client.login('MzY1NjQ5OTE4MTQyMDU0NDEw.DLkWlw.bANvZcyQXhYWXvxg5pvHBflAuss');
