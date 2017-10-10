fe643f4468e4d0de166282313f7bfc7baac0fc63
Incrested <30969346+Incrested@users.noreply.github.com>
2017-10-09 18:02:10 -0700

    Added Status Changer

--- bot.js
+++ bot.js
@@ -3,6 +3,8 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
 	console.log('Logged in as ${client.user.tag}!');
+	bot.user.setPresence({status: 'online', game: { name: 'Stufz. Do $cmds'} });
+	
 });
 
 client.on('message', msg => {
