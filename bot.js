2fe8f923c9e4c26861340892fe1cef3df10c8313
ultraMLGcode <ultramlg1108@gmail.com>
2017-10-10 17:06:11 -0700

    Fixed status message

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
 	console.log('Logged in as Miku-Chan!');
-	client.user.setPresence({ game:'Stufz. Do $cmds' });
+	client.user.setGame('Stufz. Do $cmds') });
 	console.log('Presence Changed.');
 	console.log('-------');
 	
